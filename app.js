3206d77834d9ea157aa52abedc62a90cffab612c
Roshan-sudo <r.roshan.kr32@gmail.com>
2020-12-14 18:50:38 +0530

    Final Commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const fetchData = require('./modules/fetchData');
 
 // Root Route
 app.get('/', (req, res)=>{
-    res.render('home.ejs');
+    res.render('home');
 });
 
 // Route to Fetch Youtube Playlist 
